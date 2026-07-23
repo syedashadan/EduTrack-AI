3755cc58716d7634a74611eafe042fe043de72b5
shadan <kanwal14122006@gmail.com>
2026-07-23 13:56:35 +0530

    Part 3.3 Completed

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from config import Config
-from models import db, Student
+from models import db, User, Student
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 with app.app_context():
     db.create_all()
@@ -15,6 +26,16 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
